32b3ab276f486bb35967ddd7e7144d1cc1cbd9c4
Diego <diegoboyphone@gmail.com>
2022-11-11 17:22:14 -0700

    finished off radio

--- README.md
+++ README.md
@@ -11,3 +11,9 @@ Requirements
 ## Starting the bot
 
 ```node bot.js ```
+
+
+## Debugging
+
+Get an error about (Error: Error loading tweetnacl/libsodium, voice not available)
+Fix: `npm i libsodium` and `ffmpeg` is required for the radio service to run.
--- bot.js
+++ bot.js
@@ -3,7 +3,31 @@ const secret = require('./config.json')
 const bot = new Eris(secret.discord_token);
 
 
-bot.on('ready', () => console.log(`[SA-Toolkit]: ${bot.user.username}#${bot.user.discriminator} is online`));
+bot.on('ready', async () => {
+
+	try {
+		console.log(`[SA-Toolkit]: ${bot.user.username}#${bot.user.discriminator} is online`);
+
+		await bot.createCommand({
+			name: "radio",
+			type: Eris.Constants.ApplicationCommandTypes.CHAT_INPUT,
+			description: "Listen to San Andreas radio stations (Currently only Radio Los Santos)"
+		});
+	} catch(err) {
+		console.error(`[SA-Toolkit]: ${err}`);
+	};
+});
+
+bot.on("interactionCreate", async interaction => {
+	if(interaction instanceof Eris.CommandInteraction) {
+		if(interaction.data.name == "radio") {
+			if(!interaction.member.voiceState.channelID) return interaction.createMessage("You must be inside a voice channel");
+			const vc = await bot.joinVoiceChannel(interaction.member.voiceState.channelID);
+		   	await vc.play("http://localhost/radiolossantos.mp3");
+		   	interaction.createMessage(`<#${interaction.member.voiceState.channelID}>: Radio Los Santos`)
+		}
+	}
+});
 
 /*
 bot.on('messageCreate', (message) => {
